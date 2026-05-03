c741d8ba93f4ff60a6643d89d0a35e0ddf9771ed
Vianology <oliagbenonwossi@gmail.com>
2026-05-03 14:53:31 +0000

    vite config file modified

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/coopledger-tg-28",
+  base: "/",
   server: {
     host: true
   }
